9027d8172504a7ad35bffa27ad1075f34f479139
David Correa <fortniterenegeraider@gmail.com>
2025-04-24 20:41:55 -0500

    Change the font weight of user description

--- src/components/UserDescription.tsx
+++ src/components/UserDescription.tsx
@@ -7,7 +7,7 @@ interface UserDescriptionProps {
 export const UserDescription = ({ classmate }: UserDescriptionProps) => {
   return (
     <div className="w-md h-fit dark:bg-gray-800 rounded-xl p-6 border-2 dark:border-gray-700">
-      <h1 className="mb-0 text-2xl text-gray-200">Descripción</h1>
+      <h1 className="mb-0 text-2xl text-gray-200 font-semibold">Descripción</h1>
       <p className="text-gray-300 text-[16px] mt-2">{classmate.description}</p>
     </div>
   );
