52eec9ff0c4fc09c551a79df15663c3d0a5eae51
David Correa <fortniterenegeraider@gmail.com>
2025-04-24 20:40:59 -0500

    Customizing the Badge component

--- src/shared/components/Badge.tsx
+++ src/shared/components/Badge.tsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react";
+import { randomColor } from "../../helpers/randomColor";
+
 interface BadgeProps {
   text: string;
   color?: string;
 }
 
-export const Badge = ({ text, color = "#455975" }: BadgeProps) => {
+export const Badge = ({ text }: BadgeProps) => {
+  const [color, setColor] = useState("#545454");
+  useEffect(() => {
+    setColor(randomColor());
+  }, [text]);
+
   return (
     <a
-      style={{ backgroundColor: color }}
-      className={`inline-flex items-center mr-2 px-3 py-2 mb-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 select-none`}
+      style={{
+        backgroundColor: "transparent",
+        color,
+        borderColor: color,
+      }}
+      className={`inline-flex items-center mr-2 px-3 py-2 mb-2 text-sm font-medium text-center rounded-lg focus:ring-4 select-none w-fit h-fit border-2`}
     >
       {text}
     </a>
