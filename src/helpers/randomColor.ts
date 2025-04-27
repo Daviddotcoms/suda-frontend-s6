4f0d5a2753ed61cedcf6eb21ad7fbfc35f4fb0e9
David Correa <fortniterenegeraider@gmail.com>
2025-04-27 02:08:05 -0500

    Update randomColor.ts

--- src/helpers/randomColor.ts
+++ src/helpers/randomColor.ts
@@ -1,8 +1,11 @@
+import { preference } from "./colorSchemePreference";
+
 export function randomColor(): string {
-  const light = "9abcdef";
+  let colorScheme = "9abcdef";
+  if (!preference) colorScheme = "012345678";
   let color = "#";
   for (let i = 0; i < 6; i++) {
-    color += light[Math.floor(Math.random() * 7)];
+    color += colorScheme[Math.floor(Math.random() * 7)];
   }
   return color;
 }
