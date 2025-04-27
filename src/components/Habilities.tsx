27a58a5c67619093212f57c5c9b8340cd10150e5
David Correa <fortniterenegeraider@gmail.com>
2025-04-27 02:08:32 -0500

    Make the light color for habilities & description components

--- src/components/Habilities.tsx
+++ src/components/Habilities.tsx
@@ -7,13 +7,13 @@ interface HabilitiesProps {
 
 export const Habilities = ({classmate}: HabilitiesProps) => {
     return (
-        <div className="mx-auto w-md h-fit rounded-xl p-6">
-            <h1 className="mb-6 text-2xl text-gray-200 font-semibold">
-                Habilidades
-            </h1>
-            {classmate.habilities.map((h) => (
-                <Badge text={h} key={self.crypto.randomUUID()} />
-            ))}
-        </div>
-    )
+      <div className="mx-auto w-md h-fit rounded-xl py-6">
+        <h1 className="mb-6 text-2xl dark:text-gray-200 text-black font-semibold">
+          Habilidades
+        </h1>
+        {classmate.habilities.map((h) => (
+          <Badge text={h} key={self.crypto.randomUUID()} />
+        ))}
+      </div>
+    );
 } 
--- src/components/UserDescription.tsx
+++ src/components/UserDescription.tsx
@@ -7,8 +7,12 @@ interface UserDescriptionProps {
 export const UserDescription = ({ classmate }: UserDescriptionProps) => {
   return (
     <div className="w-md h-fit dark:bg-gray-800 rounded-xl p-6 border-2 dark:border-gray-700">
-      <h1 className="mb-0 text-2xl text-gray-200 font-semibold">Descripción</h1>
-      <p className="text-gray-300 text-[16px] mt-2">{classmate.description}</p>
+      <h1 className="mb-0 text-2xl dark:text-gray-200 font-semibold text-black">
+        Descripción
+      </h1>
+      <p className="dark:text-gray-300 text-black text-[16px] mt-2">
+        {classmate.description}
+      </p>
     </div>
   );
 };
